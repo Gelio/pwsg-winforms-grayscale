ecd14d4e01626d208303f6a61ab03f33bdf5e185
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-04-05 20:35:59 +0200

    initial commit, add gitignore, import code from class (working lab part)

---
+++ AdaptiveGrayscaleHistogram/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace AdaptiveGrayscaleHistogram
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.loadImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.grayscaleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.comboBoxWorker = new System.Windows.Forms.ComboBox();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadImageToolStripMenuItem,
+            this.resetToolStripMenuItem,
+            this.grayscaleToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(414, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // loadImageToolStripMenuItem
+            // 
+            this.loadImageToolStripMenuItem.Name = "loadImageToolStripMenuItem";
+            this.loadImageToolStripMenuItem.Size = new System.Drawing.Size(81, 20);
+            this.loadImageToolStripMenuItem.Text = "Load Image";
+            this.loadImageToolStripMenuItem.Click += new System.EventHandler(this.loadImageToolStripMenuItem_Click);
+            // 
+            // resetToolStripMenuItem
+            // 
+            this.resetToolStripMenuItem.Enabled = false;
+            this.resetToolStripMenuItem.Name = "resetToolStripMenuItem";
+            this.resetToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.resetToolStripMenuItem.Text = "Reset";
+            this.resetToolStripMenuItem.Click += new System.EventHandler(this.resetToolStripMenuItem_Click);
+            // 
+            // grayscaleToolStripMenuItem
+            // 
+            this.grayscaleToolStripMenuItem.Enabled = false;
+            this.grayscaleToolStripMenuItem.Name = "grayscaleToolStripMenuItem";
+            this.grayscaleToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.grayscaleToolStripMenuItem.Text = "Grayscale";
+            this.grayscaleToolStripMenuItem.Click += new System.EventHandler(this.grayscaleToolStripMenuItem_Click);
+            // 
+            // comboBoxWorker
+            // 
+            this.comboBoxWorker.FormattingEnabled = true;
+            this.comboBoxWorker.Items.AddRange(new object[] {
+            "Sync",
+            "Async"});
+            this.comboBoxWorker.Location = new System.Drawing.Point(204, 2);
+            this.comboBoxWorker.Name = "comboBoxWorker";
+            this.comboBoxWorker.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxWorker.TabIndex = 2;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox.Location = new System.Drawing.Point(0, 24);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(414, 238);
+            this.pictureBox.TabIndex = 3;
+            this.pictureBox.TabStop = false;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.progressBar.Location = new System.Drawing.Point(0, 239);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(414, 23);
+            this.progressBar.TabIndex = 4;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork_1);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged_1);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 262);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.comboBoxWorker);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Adaptive Grayscale + histogram rozwiązanie";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem loadImageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem resetToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem grayscaleToolStripMenuItem;
+        private System.Windows.Forms.ComboBox comboBoxWorker;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ AdaptiveGrayscaleHistogram/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace AdaptiveGrayscaleHistogram
+{
+    enum GrayscaleMode
+    {
+        Async,
+        Sync
+    }
+
+    public partial class Form1 : Form
+    {
+        private Bitmap initialBitmap = null;
+        private bool shouldReset = false;
+        private GrayscaleMode grayscaleMode = GrayscaleMode.Async;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            comboBoxWorker.SelectedIndex = 0;
+        }
+
+        private void loadImageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog selectFile = new OpenFileDialog();
+            if (selectFile.ShowDialog() == DialogResult.Cancel)
+                return;
+
+            using (Stream imageStream = selectFile.OpenFile())
+            {
+                try
+                {
+                    initialBitmap = new Bitmap(imageStream);
+                } catch
+                {
+                    MessageBox.Show("Cannot load image");
+                    return;
+                }
+            }
+
+
+            pictureBox.Image = initialBitmap;
+            resetToolStripMenuItem.Enabled = true;
+            grayscaleToolStripMenuItem.Enabled = true;
+            this.MaximumSize = initialBitmap.Size;
+            this.Size = initialBitmap.Size;
+        }
+
+        private void resetToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            grayscaleToolStripMenuItem.Enabled = true;
+            if (grayscaleMode == GrayscaleMode.Sync)
+            {
+                shouldReset = true;
+                pictureBox.Image = initialBitmap;
+            }
+            else if (grayscaleMode == GrayscaleMode.Async)
+            {
+                if (backgroundWorker1.CancellationPending)
+                    return;
+
+                backgroundWorker1.CancelAsync();
+                progressBar.Value = 0;
+
+                if (!backgroundWorker1.IsBusy)
+                {
+                    pictureBox.Image = initialBitmap;
+                    return;
+                }
+            }
+            
+        }
+
+        private GrayscaleMode ParseGrayscaleMode()
+        {
+            if (comboBoxWorker.SelectedIndex == 0)
+                return GrayscaleMode.Sync;
+            else
+                return GrayscaleMode.Async;
+        }
+
+        private void grayscaleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            grayscaleToolStripMenuItem.Enabled = false;
+            grayscaleMode = ParseGrayscaleMode();
+            
+            if (grayscaleMode == GrayscaleMode.Sync)
+            {
+                StartGrayscaleSync();
+            }
+            else if (grayscaleMode == GrayscaleMode.Async)
+            {
+                if (backgroundWorker1.IsBusy)
+                    return;
+
+                pictureBox.Image = new Bitmap(initialBitmap);
+                backgroundWorker1.RunWorkerAsync();
+            }
+        }
+
+        private void StartGrayscaleSync()
+        {
+            shouldReset = false;
+            pictureBox.Image = new Bitmap(initialBitmap);
+            Bitmap bm = pictureBox.Image as Bitmap;
+
+            for (int x = 0; x < bm.Width; x++)
+            {
+                for (int y = 0; y < bm.Height; y++)
+                {
+                    if (shouldReset)
+                    {
+                        shouldReset = false;
+                        return;
+                    }
+
+                    Color baseColor = bm.GetPixel(x, y);
+                    bm.SetPixel(x, y, grayscaleColor(baseColor));
+                }
+            }
+        }
+
+        private Color grayscaleColor(Color baseColor)
+        {
+            int endColor = (int)(baseColor.R * 0.3 + baseColor.G * 0.59 + baseColor.B * 0.11);
+            return Color.FromArgb(endColor, endColor, endColor);
+        }
+
+        private void backgroundWorker1_DoWork_1(object sender, DoWorkEventArgs e)
+        {
+            Bitmap bm = new Bitmap(initialBitmap);
+            long totalPixels = bm.Width * bm.Height;
+            long currentPixel = 0;
+
+            for (int x = 0; x < bm.Width; x++)
+            {
+                for (int y = 0; y < bm.Height; y++)
+                {
+                    if (backgroundWorker1.CancellationPending)
+                    {
+                        e.Cancel = true;
+                        return;
+                    }
+
+                    currentPixel++;
+                    Color baseColor = bm.GetPixel(x, y);
+                    bm.SetPixel(x, y, grayscaleColor(baseColor));
+                }
+                int progress = (int)((double)(currentPixel) / (double)(totalPixels) * 100);
+                backgroundWorker1.ReportProgress(progress, bm);
+                Thread.Sleep(10);
+            }
+
+            pictureBox.Image = bm;
+        }
+
+        private void backgroundWorker1_ProgressChanged_1(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar.Value = e.ProgressPercentage;
+            pictureBox.Image = e.UserState as Image;
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted_1(object sender, RunWorkerCompletedEventArgs e)
+        {
+            progressBar.Value = 100;
+            grayscaleToolStripMenuItem.Enabled = true;
+        }
+    }
+}
